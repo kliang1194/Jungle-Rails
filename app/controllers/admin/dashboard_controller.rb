634990399fb67aab5ba89efe9b6a7f0d5d8b1315
Kyle <kliang1194@gmail.com>
2022-01-05 13:56:46 -0500

    Implement Admin Basic Authentication Security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+ http_basic_authenticate_with name: END["ADMIN_USERNAME_KEY"], password: ENV["ADMIN_PASSWORD_KEY"]
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: END["ADMIN_USERNAME_KEY"], password: ENV["ADMIN_PASSWORD_KEY"]
 
   def index
     @products = Product.order(id: :desc).all
